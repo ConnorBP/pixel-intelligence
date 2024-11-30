52a90131102697ea39a2c7119cd8fe431834854a
segfault <connor.postma@gmail.com>
2024-11-30 12:08:13 -0500

    Creaated useLocalStorage hook

---
+++ frontend/src/hooks/useLocalStorage.js
@@ -0,0 +1,23 @@
+// custom hook which extends slightly on useState
+// to make the data persist across browser refreshes
+// based on design by logrocket
+
+import {useState, useEffect } from "react";
+
+function getStorageValue(key, defaultValue) {
+    const saved = localStorage.getItem("key");
+    const initial = JSON.parse(saved);
+    return initial || defaultValue;
+}
+
+export const useLocalStorage = (key, defaultValue) => {
+    const [value, setValue] = useState(() => {
+        // override the defaulting behaviour to fetch local storage first 
+        return getStorageValue(key, defaultValue);
+    });
+    useEffect(() => {
+        // update storage on change
+        localStorage.setItem("key", JSON.stringify(value))
+    }, [key, value]);
+    return [value, setValue];
+}
