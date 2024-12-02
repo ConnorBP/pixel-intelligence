d92ecb5c0d1d21044ec6d9bc9b23a8279e3d9386
ConnorBP <connor.postma@gmail.com>
2024-12-02 12:23:27 -0500

    Converted context menu options to a mappable array

--- frontend/src/pages/Editor.jsx
+++ frontend/src/pages/Editor.jsx
@@ -3,14 +3,25 @@ import EditorLeftToolBar from "../components/EditorLeftToolBar";
 import EditorTopBar from "../components/EditorTopBar";
 import "../css/EditorPageCSS/Editor.css";
 import { useLocalStorage } from "../hooks/useLocalStorage";
+import { useNavigate } from "react-router-dom";
 
 const Editor = () => {
   const [brushColor, setBrushColor] = useLocalStorage("primaryBrushColor", "#000000");
   const [secondaryBrushColor, setSecondaryBrushColor] = useLocalStorage("secondaryBrushColor", "#FFFFFF");
+  const nav = useNavigate();
+
+  const contextMenuOptions = [
+    { text: "New Drawing", onClick: ()=> alert('todo') },
+    { text: "Open", onClick: ()=> alert('todo') },
+    { text: "View Gallery", onClick: () => nav("/") },
+    { text: "Share", onClick: ()=> alert('todo') },
+    { text: "Open", onClick: ()=> alert('todo') },
+    { text: "Download", onClick: ()=> alert('todo') },
+  ];
 
   return (
     <div className="editor-container">
-      <EditorTopBar />
+      <EditorTopBar contextMenuOptions={contextMenuOptions}/>
       <EditorLeftToolBar
         selectedColor={brushColor}
         setSelectedColor={setBrushColor}
