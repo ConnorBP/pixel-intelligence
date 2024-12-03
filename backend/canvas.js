0ef1c54beeac9f8f61a919aa28c4c893b4e3f9a5
sanjeev-cs <sanjeevchauhan3090@gmail.com>
2024-12-03 16:53:08 -0500

    Moved database connection
    Moved database connection from gallery.js to canvas.js

---
+++ backend/canvas.js
@@ -0,0 +1,57 @@
+const {MongoClient}  = require("mongodb");
+require("dotenv").config();
+
+const dbStringURL = process.env.MONGO_DB_STRING; // Database connection string
+const dbName = "pixel-adventure"; // Database Name
+const collectionName = "canvases"; // Collection Name
+
+// Database connection
+const connectToDB = async () => {
+    const client = new MongoClient(dbStringURL);
+    try {
+        await client.connect();
+        console.log("Connected to database");
+        return client.db(dbName); // Return the connected database instance
+    } catch (error) {
+        console.error("Failed to connect to the database:", error);
+        throw new Error("Database connection failed");
+    }
+};
+
+// Saving the canvas into the database
+const saveCanvasData = async (canvasData) => {
+    let db;
+    try {
+        db = await connectToDB();
+        const collection = db.collection(collectionName);
+
+        // Insert canvas data into the collection
+        const result = await collection.insertOne(canvasData);
+        console.log("Canvas data inserted.", result.insertedId);
+        return result; 
+    } catch (error) {
+        console.error("Error inserting canvas data:", error);
+        throw new Error("Failed to insert canvas data");
+    } finally {
+        if (db) db.client.close(); // Close the database connection
+    }
+};
+
+const getAllCanvases = async () => {
+    let db;
+    try {
+        db = await connectToDB();
+        const collection = db.collection(collectionName);
+
+        // Retrieve all canvases from the collection
+        const canvases = await collection.find().toArray();
+        return canvases; // Return the array of canvases
+    } catch (error) {
+        console.error("Error retrieving canvases:", error);
+        throw new Error("Failed to retrieve canvases");
+    } finally {
+        if (db) db.client.close();
+    }
+};
+
+module.exports = { saveCanvasData, getAllCanvases};
--- backend/start.js
+++ backend/start.js
@@ -1,5 +1,6 @@
 const app = require('./app');
 
+// Start the server on port 3000
 const server = app.listen(3000, () => {
     console.log(`Express is running on port ${server.address().port}`);
 });
