e2062a1a78a83f9c801a6c928ef94d88f8a884aa
sanjeev-cs <sanjeevchauhan3090@gmail.com>
2024-12-03 16:53:08 -0500

    Moved database connection
    Moved database connection from gallery.js to canvas.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const routes = require('./routes/index');
 
 const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
 app.use('/', routes);
+
 
 module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const galleryRoutes = require("./gallery/gallery");
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('It works!');
 });
+
+
+// Gallery routes for the "/gallery" endpoint
+router.use("/gallery", galleryRoutes);
 
 module.exports = router;
