970be57bd24d180f20a24dfc6e2a4010808b8b7c
sanjeev-cs <sanjeevchauhan3090@gmail.com>
2024-12-04 11:20:33 -0500

    removed example.env, updated file to ES module
    Removed the example.env file, also update the canvas.js, gallery.js and index.js for ES module.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,14 +1,14 @@
+//const express = require('express');
 import express from "express";
 import galleryRoutes from "./gallery/gallery.js";
-
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('It works!');
 });
 
-
 // Gallery routes for the "/gallery" endpoint
 router.use("/gallery", galleryRoutes);
+
 
 export default router;
