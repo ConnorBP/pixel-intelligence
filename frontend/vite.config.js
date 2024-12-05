419dac2bb82501ca92d167892dc77a7b3e4c8ac5
ConnorBP <connor.postma@gmail.com>
2024-12-05 14:02:22 -0500

    config changes to dist folder

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    
+    outDir: '../dist',
     rollupOptions: {
-      input: './index.html', // Ensure this path is correct
+      input: './index.html',
     },
   },
 })
