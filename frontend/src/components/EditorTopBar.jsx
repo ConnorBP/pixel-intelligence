4696f307bb8a1c9fe7e00680f63a0f5c026b7ce3
ConnorBP <connor.postma@gmail.com>
2024-12-05 10:45:24 -0500

    added more icons to top toolbar

--- frontend/src/components/EditorTopBar.jsx
+++ frontend/src/components/EditorTopBar.jsx
@@ -4,7 +4,7 @@ import NewImagePopup from "../components/NewImagePopup";
 import { MdOutlineAddBox } from "react-icons/md";
 import { RiArrowGoBackLine, RiSave3Line } from "react-icons/ri";
 import { GiResize } from "react-icons/gi";
-import { FaFileImport } from "react-icons/fa6";
+import { FaFileImport, FaShareFromSquare, FaTrashCan, FaRegFolderOpen } from "react-icons/fa6";
 import { IoIosDownload } from "react-icons/io";
 import "../css/EditorPageCSS/EditorTopBar.css";
 import Menu from "./Menu";
@@ -49,21 +49,34 @@ const EditorTopBar = ({ contextMenuOptions, onResizeImageRequested, onImportImag
       <div className="top-toolbar">
         <Menu menuOptions={contextMenuOptions} />
 
+        {/* save json file to disc */}
         <button onClick={() => { setShowSaveNotYetImplemented(true) }}>
           <RiSave3Line className="icon" />
         </button>
+        {/* import a canvas data json from disc */}
+        <button onClick={() => { setShowSaveNotYetImplemented(true) }}>
+          <FaRegFolderOpen className="icon" />
+        </button>
+        {/* import an image from disc */}
         <button onClick={onImportImageClicked}>
           <FaFileImport className="icon" />
         </button>
+        {/* create new image generation and canvas */}
         <button onClick={() => setShowNewImagePrompt(true)}>
           <MdOutlineAddBox className="icon" />
         </button>
+        {/* resize canvas */}
         <button onClick={() => setShowResizePrompt(true)}>
           <GiResize className="icon" />
         </button>
+        {/* export image */}
         <button onClick={onImageExportClicked}>
           <IoIosDownload className="icon" />
         </button>
+        {/* clear canvas */}
+        <button><FaTrashCan /></button>
+        {/* share */}
+        <button><FaShareFromSquare /></button>
 
         <button className="align-end" onClick={() => navigate("/")}>
           <RiArrowGoBackLine className="icon" /> Back to Gallery
