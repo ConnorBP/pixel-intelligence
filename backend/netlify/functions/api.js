b1468769a86a5ca934d19128d72f7f13e15fd688
ConnorBP <connor.postma@gmail.com>
2024-12-05 14:25:12 -0500

    changing api endpoint temporarily to test netlify

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -2,15 +2,23 @@
 
 import express from "express";
 import serverless from "serverless-http";
-import routes from '../routes/index.js';
+// import routes from '../routes/index.js';
 
 const api = express();
 
 // Middleware to parse JSON
 api.use(express.json());
 
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('It (sort of) works!');
+});
+
+api.use("/api/", router);
+
 // add the api routes
-api.use("/api/", routes);
+// api.use("/api/", routes);
 
 // export the handler to netlify
 export const handler = serverless(api);
