be92e2672a4b2ac86625c0366865f1a158afd0f1
Parmilashams <pshams@my.centennialcollege.ca>
2024-12-06 14:44:55 -0500

    More details to the images

--- frontend/src/components/GalleryPageComponents/ImageItem.jsx
+++ frontend/src/components/GalleryPageComponents/ImageItem.jsx
@@ -1,11 +1,14 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
-// Component to render an individual image item
 export const ImageItem = ({ image }) => {
     return (
         <div className="images">
             <Link to={`/gallery/viewImage/${image.id}`}>
                 <img src={image.imgSrc} alt={`Gallery Image ${image.id}`} style={{ width: "100%" }} />
+            </Link>
+            <Link to={`/gallery/details/${image.id}`} className="details_button">
+                More Details
             </Link>
         </div>
     );
