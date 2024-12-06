6b5d431bb2ec6fb2711fb872a18752c118b6bc30
segfault <connor.postma@gmail.com>
2024-12-05 20:43:26 -0500

    bugfix to environment variable handling

--- backend/routes/gallery/gallery.js
+++ backend/routes/gallery/gallery.js
@@ -2,8 +2,6 @@ import express from "express";
 import { validateCanvasData } from './validator.js';
 import {saveCanvasData, getAllCanvases} from "./canvas.js";
 import { authenticate } from "./authentication.js";
-import "dotenv/config";
-
 
 const router = express.Router();
 
