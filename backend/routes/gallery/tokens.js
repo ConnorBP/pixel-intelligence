5a0e398f4553a9e98010348cdc69e6dbca2f75e2
ConnorBP <connor.postma@gmail.com>
2024-12-06 00:04:36 -0500

    updated timeout constants for webtoken

--- backend/routes/gallery/tokens.js
+++ backend/routes/gallery/tokens.js
@@ -15,7 +15,7 @@
       // Create a token with user IP
       // console.log(process.env.SECRET_KET);
       let key = process.env.SECRET_KET;
-      let expiresIn = 60 * 60 * 24; // 24 hours
+      let expiresIn = 60 * 60 * 24; // 24 hours in seconds
       const token = jwt.sign({ ip: userIP }, key, { expiresIn: expiresIn  });
       console.log("ip: "+ userIP);
       console.log("access token: " + token);
@@ -24,8 +24,8 @@
       try{
         res.cookie("access_token", token, {
           httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          maxAge: 86400000,  // 1 day
+          secure: process.env.NODE_ENV === "production" || process.env.SECURE_MODE == true,
+          maxAge: 60 * 60 * 24 * 1000,  // 24 hours in milliseconds
           path: "/"
         });
         console.log("Cookie Generated");
