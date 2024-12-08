0ed37cbdcb4faa7b87b7212dfa455f120153d277
ConnorBP <connor.postma@gmail.com>
2024-12-07 20:39:00 -0500

    got session state working on react

--- frontend/src/context/SessionContext.jsx
+++ frontend/src/context/SessionContext.jsx
@@ -1,35 +1,89 @@
-import { createContext } from 'react';
+import { createContext, useState, useContext, useEffect } from 'react';
+import { getApiEndpoint } from '../utils';
 // import { useLocalStorage } from '../hooks/useLocalStorage';
-import Cookies from 'js-cookie';
+// import Cookies from 'js-cookie';
 
 const SessionContext = createContext(null);
 
 export const SessionProvider = ({ children }) => {
     // const [currentSession, setCurrentSession] = useLocalStorage("userSession",null);
+    const [sessionLoaded, setSessionLoaded] = useState(false);
+
+    // the time in NumericDate format at which the session will expire in utc seconds since epoch (Jan 1, 1970)
+    // NumericDate: A JSON numeric value representing the number of seconds from 1970-01-01T00:00:00Z UTC
+    // until the specified UTC date/time, ignoring leap seconds.
+    const [sessionExpiry, setSessionExpiry] = useState(0);
 
     const getCurrentSession = () => {
-        const currentToken = Cookies.get('access_token');
+        // const currentToken = Cookies.get('access_token');
 
-        return 
+        return
     }
 
     const refreshSession = () => {
-        //todo fetch new session from api endpoint
+        setSessionLoaded(false);
+
+        const endpoint = getApiEndpoint() + '/auth';
+        console.log(endpoint);
+        fetch(endpoint, {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to fetch session');
+                }
+                return response.json();
+            })
+            .then(data => {
+                setSessionExpiry(data.expiresAt);
+                setSessionLoaded(true);
+            })
+            .catch(error => {
+                console.error('Error fetching session:', error);
+                setSessionLoaded(false);
+            });
     };
+
+    // refresh session once on load if unset
+    useEffect(() => {
+        if (!sessionLoaded) {
+            refreshSession();
+        }
+    }, []);
+
+
+    function checkExpired() {
+        // if the session is expired
+        if (sessionExpiry < Math.floor(Date.now() / 1000)) {
+            // clear the session
+            // setCurrentSession(null);
+            setSessionLoaded(false);
+
+            return false;
+        }
+        return true;
+    }
 
 
     return (
-        <SessionContext.Provider value={{ getCurrentSession, refresh: refreshSession }}>
+        <SessionContext.Provider value={{
+            sessionLoaded,
+            sessionExpiry,
+            refresh: refreshSession,
+            sessionExpired: checkExpired
+        }}>
             {children}
         </SessionContext.Provider>
     );
 };
 
-export const useSession = () => {
-    const { currentSession, refresh } = useContext(SessionContext);
+export function useSession() {
+    const { sessionLoaded, sessionExpiry, refresh, sessionExpired } = useContext(SessionContext);
 
-
-
-    return { currentSession, refresh };
+    return { sessionLoaded, sessionExpiry, refresh, sessionExpired };
 };
 
