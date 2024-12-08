939917d955055977c684a9f8f09c122d089fa35e
ConnorBP <connor.postma@gmail.com>
2024-12-08 02:16:48 -0500

    fixed pagination number display shifting

--- frontend/src/components/GalleryPageComponents/GalleryPagination.jsx
+++ frontend/src/components/GalleryPageComponents/GalleryPagination.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../../css/GalleryPageCSS/GalleryPagination.css';
 
-const GalleryPagination = ({ currentPage = 1, totalPages = 999, onPageSelected }) => {
+const GalleryPagination = ({ currentPage = 1, totalPages = 999, numberOfButtons = 5, onPageSelected }) => {
+
     const handlePageClick = (page) => {
-        if (onPageSelected && page >= 1 && page <= 8) {
+        if (onPageSelected && page >= 1 && page <= totalPages) {
             onPageSelected(page);
         }
     };
+
+    // calculate where to display the base page number from in the list
+    const basePage = Math.max(1, Math.min(currentPage - Math.floor(numberOfButtons / 2), totalPages - numberOfButtons + 1));
 
     return (
         <div className="pagination">
@@ -17,13 +21,14 @@ const GalleryPagination = ({ currentPage = 1, totalPages = 999, onPageSelected }
             >
                 Back
             </button>
-            {[...Array(totalPages).keys()].slice(0, 5).map((_, index) => (
+            {[...Array(totalPages).keys()].slice(basePage - 1, basePage + 4).map((v) => (
+
                 <button
-                    key={index + 1}
-                    onClick={() => handlePageClick(index + 1)}
-                    className={currentPage === index + 1 ? 'active' : ''}
+                    key={v + 1}
+                    onClick={() => handlePageClick(v + 1)}
+                    className={currentPage === v + 1 ? 'active' : ''}
                 >
-                    {index + 1}
+                    {v + 1}
                 </button>
             ))}
             <button
