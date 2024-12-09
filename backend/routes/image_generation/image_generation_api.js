3a482c962bdf968fa174ba85d78e4c626866c23e
ConnorBP <connor.postma@gmail.com>
2024-12-08 20:16:14 -0500

    improved generate image endpoint parameter checking

--- backend/routes/image_generation/image_generation_api.js
+++ backend/routes/image_generation/image_generation_api.js
@@ -15,20 +15,38 @@ const isValidJobId = (jobId) => {
 // Header parameters
 const header = {
   headers: {
-      "accept": "application/json",
-      "apikey": apiKey,
-      "Content-Type": "application/json",
+    "accept": "application/json",
+    "apikey": apiKey,
+    "Content-Type": "application/json",
   }
 }
 
-// POST route to start image generation
-router.post("/generate/:userPrompt/:pixelSize", async(req, res) => {
-  const { userPrompt, pixelSize } = req.params;
+const validSizes = [8, 16, 32, 64];
 
-  // Check if user prompt and pixel size is their
-  if (!userPrompt || !pixelSize) {
-    return res.status(400).json({ success: false, error: "Invalid request data" });
+// POST route to start image generation
+router.post("/generate", async (req, res) => {
+  // optional prompt and pixel size query parameters: ?promt=pixel+art&size=16
+  // if not provided defaults to pixel art and 16px
+  const { prompt, size } = req.query || { prompt: "pixel art", size: 16 };
+
+  if (!validSizes.includes(parseInt(size))) {
+    return res.status(400).json({ success: false, error: "Invalid pixel size. Valid sizes are 8, 16, 32, 64." });
   }
+
+  // we don't go lower than 256px because then the images turn out a bloody mess
+  const minimumGenerationSize = 256;
+
+  // stable diffusion generates 8 pixels per pixel,
+  // so we request 8 times the end goal resolution
+  // also we cap the lower bounds at 256 so the result isn't garbage
+
+  // 8px canvas: 256 generated pixels
+  // 16px canvas: 256 generated pixels
+  // 32px canvas: 256 generated pixels
+  // 64px canvas: 512 generated pixels
+  // 128px canvas: 1024 generated pixels
+
+  const gnerationResolution = Math.max(pixelSize * 8, minimumGenerationSize);
 
   // Job ID for each job
   const jobId = uuidv4();
@@ -40,8 +58,8 @@ router.post("/generate/:userPrompt/:pixelSize", async(req, res) => {
       "cfg_scale": 7,
       "seed": "493768514",
       "sampler_name": "k_euler_a",
-      "height": 512,
-      "width": 512,
+      "height": gnerationResolution,
+      "width": gnerationResolution,
       "post_processing": [],
       "steps": 10,
       "tiling": false,
@@ -53,19 +71,19 @@ router.post("/generate/:userPrompt/:pixelSize", async(req, res) => {
     "allow_downgrade": false,
     "nsfw": false,
     "censor_nsfw": true,
-    "trusted_workers": true,
+    "trusted_workers": false,
     "models": [
       "AIO Pixel Art"
     ],
     "r2": true,
     "replacement_filter": true,
-    "shared": false,
+    "shared": true,
     "slow_workers": false,
     "dry_run": false
   }
 
   // Making request to the api for image id
-  try{
+  try {
     const response = await axios.post(`${baseUrl}/generate/async`, data, header);
 
     // Saving the data in the database
@@ -78,15 +96,15 @@ router.post("/generate/:userPrompt/:pixelSize", async(req, res) => {
     });
 
     res.status(202).json({ success: true, jobId });
-  } catch(e) {
-      console.error("Error starting generation: "+ e.response ? e.response.data : e.message);
-      res.status(500).json({ success: false, error: "Internal server error" });
+  } catch (e) {
+    console.error("Error starting generation: " + e.response ? e.response.data : e.message);
+    res.status(500).json({ success: false, error: "Internal server error" });
   }
 
 });
 
 // GET request to poll generation status
-router.get("/poll/:jobId", async(req, res) => {
+router.get("/poll/:jobId", async (req, res) => {
   const { jobId } = req.params;
   // console.log("Job ID: " + jobId);
 
@@ -95,7 +113,7 @@ router.get("/poll/:jobId", async(req, res) => {
     return res.status(400).json({ success: false, error: "Invalid job Id." });
   }
 
-  try{
+  try {
     const imageJob = await getImageJobData(jobId);
 
     if (!imageJob) {
@@ -104,20 +122,20 @@ router.get("/poll/:jobId", async(req, res) => {
 
     const response = await axios.get(`${baseUrl}/generate/status/${imageJob.generationId}`, header);
 
-    if(response.data.done) {
+    if (response.data.done) {
       await updateImageJobStatus(jobId, "completed", response.data.generations[0].img);
       return res.status(200).json({ success: true, status: "completed" });
     }
 
     res.status(200).json({ success: true, status: response.data });
-  } catch(e) {
+  } catch (e) {
     console.error("Error polling status: ", e.response ? e.response.data : e.message);
     res.status(500).json({ success: false, error: "Internal server error" });
   }
 });
 
 // GET request to download the image
-router.get("/download/:jobId", async(req, res) => {
+router.get("/download/:jobId", async (req, res) => {
   const { jobId } = req.params;
   console.log("Job ID: " + jobId);
   // Checking if the job id format is valid or not
@@ -125,14 +143,14 @@ router.get("/download/:jobId", async(req, res) => {
     return res.status(400).json({ success: false, error: "Invalid job Id." });
   }
 
-  try{
+  try {
     const imageJob = await getImageJobData(jobId);
 
-    if(imageJob.status !== "completed") {
-      return res.status(404).json({success : false, error: "Image is not ready yet."});
+    if (imageJob.status !== "completed") {
+      return res.status(404).json({ success: false, error: "Image is not ready yet." });
     }
     res.redirect(imageJob.downloadURL);
-  } catch(e) {
+  } catch (e) {
     console.error("Error downloading image: ", e.response ? e.response.data : e.message);
     res.status(500).json({ success: false, error: "Internal server error" });
   }
