2a35316f9bf1c741e0572d3e7db9c4a43905dce6
ConnorBP <connor.postma@gmail.com>
2024-12-09 13:29:16 -0500

    eslint now properly warns about case sensitive imports

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,52 +1,45 @@
-import js from '@eslint/js';
-import globals from 'globals';
+import { defineConfig } from 'eslint-define-config';
+// import js from '@eslint/js';
+// import globals from 'globals';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import importPlugin from 'eslint-plugin-import';
+import cssModules from 'eslint-plugin-css-modules';
 
-export default [
-  { ignores: ['dist'] },
-  {
+export default defineConfig({
     files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
+        ecmaVersion: 2020,
         sourceType: 'module',
-      },
-    },
-    settings: { 
-      react: { version: '18.3' },
-      'import/resolver': {
-        node: {
-          caseSensitive: true
-        }
-      }
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
     },
     plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'import': importPlugin
+        react,
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
+        'import': importPlugin,
+        'css-modules': cssModules
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react/prop-types': 0,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'import/no-unresolved': [
-        'warn',
-        { caseSensitive: true }
-      ]
-    }
-  }
-];
+        'react/jsx-no-target-blank': 'off',
+        'react/prop-types': 'off',
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+        'import/no-unresolved': [
+            'error',
+            { caseSensitive: true },
+        ],
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+});
