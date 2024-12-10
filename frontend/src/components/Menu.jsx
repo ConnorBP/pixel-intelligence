3ccb134cb41f7b7d0f44793b3fb3a8d5f6444be3
sanjeev-cs <sanjeevchauhan3090@gmail.com>
2024-12-10 12:21:13 -0500

    modified Menu.jsx to handle click outside the menu
    Updated the Menu.jsx so that the menu is close when clicked outside.

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { IoMdMenu } from "react-icons/io";
 import "../css/Menu.css";
 import ContextMenuItem from "./ContextMenuItem";
 
-const testingItems= [
-  { text: "New Todo", onClick: ()=> alert('todo') },
-  { text: "DEMO OPTION", onClick: ()=> alert('todo') },
-  { text: "TODO#3", onClick: ()=> alert('todo') },
+const testingItems = [
+  { text: "New Todo", onClick: () => alert('todo') },
+  { text: "DEMO OPTION", onClick: () => alert('todo') },
+  { text: "TODO#3", onClick: () => alert('todo') },
 ];
 
 // console.log(testingItems);
@@ -17,14 +17,34 @@ function Menu({ menuOptions = testingItems }) {
     setMenuOpen((v) => !v);
   };
 
-  const listItems = menuOptions.map(({text, onClick}, idx) => {
+  // Create a reference to the menu container element
+  let menuRef = useRef();
+
+  useEffect(() => {
+    // Function to handle clicks outside the menu
+    let handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        toggle();
+      }
+    };
+
+    // Add as mouse click event listener to the entire document
+    document.addEventListener("mousedown", handler);
+
+    // Remove the event listener
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    }
+  })
+
+  const listItems = menuOptions.map(({ text, onClick }, idx) => {
     // console.log(`mapping item ${idx} ${text}`);
-    return <ContextMenuItem onClick={()=>{toggle();onClick();}} key={`${text} ${idx}`}>{text}</ContextMenuItem>;
+    return <ContextMenuItem onClick={() => { toggle(); onClick(); }} key={`${text} ${idx}`}>{text}</ContextMenuItem>;
   });
   // console.log(listItems);
 
   return (
-    <div className="menu-container">
+    <div className="menu-container" ref={menuRef}>
       <button className="menu-button" onClick={toggle}>
         <IoMdMenu className="icon" />
       </button>
