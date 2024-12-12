ebd0afc6d439c4b6b8e72ea4b61c66616e680688
Parmilashams <158627604+Parmilashams@users.noreply.github.com>
2024-12-12 10:21:30 -0500

    Updated ColorPickerToolbar styles (#161)
    * Updated ColorPickerToolbar styles
    
    * Updated ColorPickerToolbar styles and other modifications
    
    * fixed sizing of color boxes on mobile scale
    
    * format colorpicker css
    
    * fixed height scale of color picker layout square to be rectangle on mobile
    
    * better styling for toolbar icons
    
    ---------
    
    Co-authored-by: Connor "segfault" Postma <connor.postma@gmail.com>

--- frontend/src/components/EditorLeftToolBar.jsx
+++ frontend/src/components/EditorLeftToolBar.jsx
@@ -14,21 +14,21 @@ const EditorLeftToolBar = ({ selectedColor, setSelectedColor, secondaryColor, se
       <div className="icon-container">
         {/* editor buttons go here */}
         <button
-          className={`one-hundo-pw icon ${tool === "pencil" ? "active" : ""} tooltip-vertical`}
+          className={`icon ${tool === "pencil" ? "active" : ""} tooltip-vertical`}
           button-name="Pencil"
           onClick={() => setTool("pencil")}><FaPencilAlt />
         </button>
         <button
-          className={`one-hundo-pw icon ${tool === "eraser" ? "active" : ""} tooltip-vertical`}
+          className={`icon ${tool === "eraser" ? "active" : ""} tooltip-vertical`}
           button-name="Eraser"
           onClick={() => { setTool("eraser") }}><FaEraser />
         </button>
         <button
-          className={`one-hundo-pw icon ${tool === "paint" ? "active" : ""} tooltip-vertical`}
+          className={`icon ${tool === "paint" ? "active" : ""} tooltip-vertical`}
           button-name="Bucket Fill"
           onClick={() => setTool("paint")} ><PiPaintBucketFill /></button>
         <button
-          className={`one-hundo-pw icon ${colorPickerActive === true ? "active" : ""} tooltip-vertical`}
+          className={`icon ${colorPickerActive === true ? "active" : ""} tooltip-vertical`}
           button-name="Color Picker"
           onClick={async () => {
             if (onEyeDropperClicked) {
@@ -41,7 +41,7 @@ const EditorLeftToolBar = ({ selectedColor, setSelectedColor, secondaryColor, se
           <FaEyeDropper />
         </button>
         <button
-          className={`one-hundo-pw icon ${gridLinesVisible ? "active" : ""} tooltip-vertical`}
+          className={`icon ${gridLinesVisible ? "active" : ""} tooltip-vertical`}
           button-name="Grid"
           onClick={toggleGridLines}>
           <MdOutlineGridOn />
